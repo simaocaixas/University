d3c6f28fe7e87aede6d7627e9faba005ab989d0d
Simão Caixas <145067642+simaocaixas@users.noreply.github.com>
2024-04-18 23:15:33 +0100

    Add files via upload

---
+++ IAED/Lab02/ex06.c
@@ -0,0 +1,28 @@
+/* iaed24 - ist1109632 - lab03/ex06 */
+#include <stdio.h>
+#include <string.h>
+#define VECMAX 100
+
+int main() {
+
+
+    char N[VECMAX];
+
+    scanf("%s", N);
+    int Tam = strlen(N);
+    int Inteiro = 0;
+    int i = 0;
+
+    while (i < Tam) {
+        Inteiro += N[i] - '0';
+        i++; 
+    }
+    if (Inteiro % 9 == 0) {
+        printf("yes\n");
+    }
+
+    else {
+        printf("no\n");
+    }
+    return 0;
+}
